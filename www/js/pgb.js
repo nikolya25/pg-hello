e0feb1aec05dd4ef4112614aadb1324b115cfe49
Mykola Shchur <kolya_shchur@mail.ru>
2017-03-26 16:04:19 +0200

    filter test

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -81,27 +81,10 @@ function onError(contactError) {
  
 function showContacts() {
 	var options      = new ContactFindOptions();
-	options.filter   = "";
+	options.filter   = "Domin";
 	options.multiple = true;
 	options.desiredFields = [navigator.contacts.fieldType.id];
 	options.hasPhoneNumber = true;
 	filter = ["displayName", "name"];
 	navigator.contacts.find(filter, onSuccess, onError, options);
-}
-
-function contactsList() {
-navigator.contactsPhoneNumbers.list(
-	function(contacts) {
-		console.log(contacts.length + ' contacts found');
-		for(var i = 0; i < contacts.length; i++) {
-			console.log(contacts[i].id + " - " + contacts[i].displayName);
-			for(var j = 0; j < contacts[i].phoneNumbers.length; j++) {
-				var phone = contacts[i].phoneNumbers[j];
-				console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
-			}
-		}
-	}, 
-	function(error) {
-		console.error(error);
-	});
 }
