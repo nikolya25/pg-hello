867acc300cb0bbf6fd9458e54413b1a4da935f81
Nikolya <kolya_shchur@mail.ru>
2017-04-18 18:11:08 +0200

    no button

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -21,24 +21,20 @@ function deviceInfo() {
 	
 }
 
-function onSuccess(position) {
-        alert('Latitude: '          + position.coords.latitude          + '\n' +
-              'Longitude: '         + position.coords.longitude         + '\n' +
-              'Altitude: '          + position.coords.altitude          + '\n' +
-              'Accuracy: '          + position.coords.accuracy          + '\n' +
-              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-              'Heading: '           + position.coords.heading           + '\n' +
-              'Speed: '             + position.coords.speed             + '\n' +
-              'Timestamp: '         + position.timestamp                + '\n');
-    };
+var onSucces = function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+        'Longitude: '         + position.coords.longitude         + '\n' +
+        'Altitude: '          + position.coords.altitude          + '\n' +
+        'Accuracy: '          + position.coords.accuracy          + '\n' +
+        'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+        'Heading: '           + position.coords.heading           + '\n' +
+        'Speed: '             + position.coords.speed             + '\n' +
+        'Timestamp: '         + position.timestamp                + '\n');
+};
  
-    // onError Callback receives a PositionError object 
-    // 
-    function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
-    }
- 
-function showLocation() {
-	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+        'message: ' + error.message + '\n');
 }
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
