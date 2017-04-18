5563c02e3f8fe2a20dda349d5c343ed67c751fa2
Mykola Shchur <kolya_shchu@mail.ru>
2017-04-19 00:19:00 +0200

    jq test

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -39,7 +39,7 @@ function onMapSuccess(position) {
  
 }*/
 
-function getMap() {
+/*function getMap() {
 	var mapOptions= {
 		center: new google.maps.LatLng(0, 0),
 		zoom: 5,
@@ -47,4 +47,35 @@ function getMap() {
 	};
 	
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
+}*/
+
+$( document ).on( "pageinit", "#map-page", function() {
+    var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
+    if ( navigator.geolocation ) {
+        function success(pos) {
+            // Location found, show map with these coordinates
+            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        }
+        function fail(error) {
+            drawMap(defaultLatLng);  // Failed to find location, show default map
+        }
+        // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
+        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
+    } else {
+        drawMap(defaultLatLng);  // No geolocation support, show default map
+    }
+    function drawMap(latlng) {
+        var myOptions = {
+            zoom: 10,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+        // Add an overlay to the map of current lat/lng
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title: "Greetings!"
+        });
+    }
+});
