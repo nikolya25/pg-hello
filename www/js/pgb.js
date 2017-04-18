d6b316625921ca42b229f2194b33baa684e3a553
Mykola Shchur <kolya_shchu@mail.ru>
2017-04-18 19:17:44 +0200

    map try

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -21,94 +21,11 @@ function deviceInfo() {
 	
 }
 
-function onSuccess(acceleration) {
-    alert('Acceleration X: ' + acceleration.x + '\n' +
-          'Acceleration Y: ' + acceleration.y + '\n' +
-          'Acceleration Z: ' + acceleration.z + '\n' +
-          'Timestamp: '      + acceleration.timestamp + '\n');
-}
-
-function onError() {
-    alert('onError!');
-}
-
-function getAcceleration() {
-	navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
-}
-
-var Latitude = undefined;
-var Longitude = undefined;
- 
-// Get geo coordinates 
- 
-function getMapLocation() {
- 
-    navigator.geolocation.getCurrentPosition
-    (onMapSuccess, onMapError, { enableHighAccuracy: true });
-}
- 
-// Success callback for get geo coordinates 
- 
-var onMapSuccess = function (position) {
- 
-    Latitude = position.coords.latitude;
-    Longitude = position.coords.longitude;
- 
-    getMap(Latitude, Longitude);
- 
-}
- 
-// Get map by using coordinates 
- 
-function getMap(latitude, longitude) {
- 
-    var mapOptions = {
-        center: new google.maps.LatLng(0, 0),
-        zoom: 1,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
- 
-    map = new google.maps.Map
-    (document.getElementById("map"), mapOptions);
- 
- 
-    var latLong = new google.maps.LatLng(latitude, longitude);
- 
-    var marker = new google.maps.Marker({
-        position: latLong
-    });
- 
-    marker.setMap(map);
-    map.setZoom(15);
-    map.setCenter(marker.getPosition());
-}
- 
-// Success callback for watching your changing position 
- 
-var onMapWatchSuccess = function (position) {
- 
-    var updatedLatitude = position.coords.latitude;
-    var updatedLongitude = position.coords.longitude;
- 
-    if (updatedLatitude != Latitude && updatedLongitude != Longitude) {
- 
-        Latitude = updatedLatitude;
-        Longitude = updatedLongitude;
- 
-        getMap(updatedLatitude, updatedLongitude);
-    }
-}
- 
-// Error callback 
- 
-function onMapError(error) {
-    console.log('code: ' + error.code + '\n' +
-        'message: ' + error.message + '\n');
-}
- 
-// Watch your changing position 
- 
-function watchMapPosition() {
- 
-    return navigator.geolocation.watchPosition(onMapWatchSuccess, onMapError, { enableHighAccuracy: true });
+function myMap() {
+	var mapOptions = {
+		center: new google.maps.LatLng(51.5, -0.12),
+		zoom: 10,
+		mapTypeId: google.maps.MapTypeId.HYBRID
+	};
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
