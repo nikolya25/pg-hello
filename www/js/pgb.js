71fb733afe6016a766f676d956aadec26e4cfe17
Mykola Shchur <kolya_shchu@mail.ru>
2017-04-19 04:36:45 +0200

    another google map plugin test

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -55,7 +55,7 @@ function deviceInfo() {
  
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
 */
-
+/*
 var Latitude = undefined;
 var Longitude = undefined;
  
@@ -111,28 +111,53 @@ function getMap(latitude, longitude) {
 }
 
 getMapLocation();
-
-const GORYOKAKU_JAPAN = {"lat": 41.796875, "lng": 140.757007};
-
-var map = plugin.google.maps.Map.getMap({
-  'backgroundColor': 'white',
-  'mapType': plugin.google.maps.MapTypeId.ROADMAP,
-  'controls': {
-    'compass': true,
-    'myLocationButton': true,
-    'indoorPicker': true,
-    'zoom': true
-  },
-  'gestures': {
-    'scroll': true,
-    'tilt': true,
-    'rotate': true,
-    'zoom': true
-  },
-  'camera': {
-    'latLng': GORYOKAKU_JAPAN,
-    'tilt': 30,
-    'zoom': 15,
-    'bearing': 50
-  }
-});
+*/
+var map;
+document.addEventListener("deviceready", function() {
+  var div = document.getElementById("map");
+ 
+  // Initialize the map view 
+  map = plugin.google.maps.Map.getMap(div);
+ 
+  // Wait until the map is ready status. 
+  map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
+}, false);
+ 
+function onMapReady() {
+  var button = document.getElementById("button");
+  button.addEventListener("click", onBtnClicked);
+}
+ 
+function onBtnClicked() {
+ 
+  // Move to the position with animation 
+  map.animateCamera({
+    target: {lat: 37.422359, lng: -122.084344},
+    zoom: 17,
+    tilt: 60,
+    bearing: 140,
+    duration: 5000
+  }, function() {
+ 
+    // Add a maker 
+    map.addMarker({
+      position: {lat: 37.422359, lng: -122.084344},
+      title: "Welecome to \n" +
+             "Cordova GoogleMaps plugin for iOS and Android",
+      snippet: "This plugin is awesome!",
+      animation: plugin.google.maps.Animation.BOUNCE
+    }, function(marker) {
+ 
+      // Show the info window 
+      marker.showInfoWindow();
+ 
+      // Catch the click event 
+      marker.on(plugin.google.maps.event.INFO_CLICK, function() {
+ 
+        // To do something... 
+        alert("Hello world!");
+ 
+      });
+    });
+  });
+}
