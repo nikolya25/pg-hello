2c49b0f6d7d12ff2d80bee33aa224b74932a434e
Mykola Shchur <kolya_shchu@mail.ru>
2017-04-25 18:15:35 +0200

    sms only

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -86,32 +86,6 @@ function showContacts () {
 	navigator.contacts.find(filter, onSuccess, onError, options);
 }*/
 
-function onSuccess(contacts) {
-    for (var i = 0; i < contacts.length; i++) {
-        for (var j = 0; j < contacts[i].addresses.length; j++) {
-            alert("Pref: "         + contacts[i].addresses[j].pref          + "\n" +
-                "Type: "           + contacts[i].addresses[j].type          + "\n" +
-                "Formatted: "      + contacts[i].addresses[j].formatted     + "\n" +
-                "Street Address: " + contacts[i].addresses[j].streetAddress + "\n" +
-                "Locality: "       + contacts[i].addresses[j].locality      + "\n" +
-                "Region: "         + contacts[i].addresses[j].region        + "\n" +
-                "Postal Code: "    + contacts[i].addresses[j].postalCode    + "\n" +
-                "Country: "        + contacts[i].addresses[j].country);
-        }
-    }
-};
- 
-function onError(contactError) {
-    alert('onError!');
-};
- 
-// find all contacts 
-var options = new ContactFindOptions();
-options.filter = "";
-options.multiple = true;
-var filter = ["displayName", "addresses"];
-navigator.contacts.find(filter, onSuccess, onError, options);
-
 var app = {
     sendSms: function() {
         var number = document.getElementById('numberTxt').value;
