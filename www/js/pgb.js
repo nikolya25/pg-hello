91f454b063272dd2bc6f1a04e152347a7e4c8583
Mykola Shchur <kolya_shchu@mail.ru>
2017-05-02 16:11:52 +0200

    dif docs

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -59,21 +59,3 @@ map.one(plugin.google.maps.event.MAP_READY, function() {
 		map.getMyLocation(onSuccess, onError);
 	});	
 });
-	
-var app = {
-    sendSms: function() {
-        var number = document.getElementById('numberTxt').value;
-        var message = document.getElementById('messageTxt').value;
-        console.log("number=" + number + ", message= " + message);
-
-        //CONFIGURATION
-        var options = {
-            replaceLineBreaks: false, // true to replace \n by a new line, false by default
-            android: {
-                intent: 'INTENT'  // send SMS with the native android SMS messaging
-                //intent: '' // send SMS without open any other app
-            }
-        };
-        sms.send(number, message, options);
-    }
-};
