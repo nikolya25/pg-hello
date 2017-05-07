f8f28b24c27b6ef5fe592a763a442b79741a28fb
Mykola Shchur <kolya_shchu@mail.ru>
2017-05-07 07:44:07 +0200

    maps

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -3,83 +3,85 @@ function init() {
 }
 
 function onDeviceReady() {
-	document.getElementById('mapshowbutton').style.display = "none";
-var div = document.getElementById("map");
-var map = plugin.google.maps.Map.getMap(div, {
-	'mapType': plugin.google.maps.MapTypeId.ROADMAP,
-	'controls': {
-		'compass': true,
-		'myLocationButton': true,
-		'indoorPicker': true,
-		'zoom': true
-	},
-	'gestures': {
-		'scroll': true,
-		'tilt': true,
-		'rotate': true,
-		'zoom': true
-	},
-	camera: {
-		target : {
-			lat: 50.0593677, lng: 19.9375843
+	var div = document.getElementById("map");
+	var map = plugin.google.maps.Map.getMap(div , {
+		'mapType': plugin.google.maps.MapTypeId.ROADMAP,
+		'controls': {
+			'compass': true,
+			'myLocationButton': true,
+			'indoorPicker': true,
+			'zoom': true
 		},
-		zoom : 14
-	}
-});
+		'gestures': {
+			'scroll': true,
+			'tilt': true,
+			'rotate': true,
+			'zoom': true
+		},
+		camera: {
+			target : {
+				lat: 50.0593677, lng: 19.9375843
+			},
+			zoom : 14
+		}
+	});
 }
-map.one(plugin.google.maps.event.MAP_READY, function() {	
-	/*var stations = [
-	{
-		position: {lng: 50,057678, lat: 19,926189},
-		title: "Kraków, Aleja Krasińskiego",
-		snippet: "Kraków, Aleja Krasińskiego" 
-	},
-	{
-		position: {lng: 50,057447, lat: 19,946008},
-		title: "Kraków, ul. Dietla",
-		snippet: "Kraków, ul. Dietla"
-	},
-	{
-		position: {lng: 50,010575, lat: 19,949189},
-		title: "Kraków, ul. Bujaka",
-		snippet: "Kraków, ul. Bujaka"
-	},
-	{
-		position: {lng: 50,081197, lat: 19,895358},
-		title: "Kraków, ul. Złoty Róg",
-		snippet: "Kraków, ul. Złoty Róg"
-	},
-	{
-		position: {lng: 50,069308, lat: 20,053492},
-		title: "Kraków, ul. Bulwarowa",
-		snippet: "Kraków, ul. Bulwarowa"
-	},
-	{
-		position: {lng: 50,099361, lat: 20,018317},
-		title: "Kraków, os. Piastów",
-		snippet: "Kraków, os. Piastów"
-	},
-	{
-		position: {lng: 50,0192 , lat: 20,016803},
-		title: "Kraków, ul. Telimeny",
-		snippet: "Kraków, ul. Telimeny"
-	},
-	{
-		position: {lng: 50,100569, lat: 20,122561},
-		title: "Kraków, os. Wadów",
-		snippet: "Kraków, os. Wadów"
-	}
-	];
-
-	map.addMarkers(map, stations, function(markers) {
-		var bounds = [];
-		stations.forEach(function(POI) {
-			bounds.push(POI.position);
-		});
-		map.moveCamera({
-			target: bounds
-		}, function() {
-			markers[markers.length - 1].showInfoWindow();
+	
+map.one(plugin.google.maps.event.MAP_READY, function() {
+	
+var stations = [
+		{
+			position: {lng: 50,057678, lat: 19,926189},
+			title: "Kraków, Aleja Krasińskiego",
+			snippet: "Kraków, Aleja Krasińskiego" 
+		},
+		{
+			position: {lng: 50,057447, lat: 19,946008},
+			title: "Kraków, ul. Dietla",
+			snippet: "Kraków, ul. Dietla"
+		},
+		{
+			position: {lng: 50,010575, lat: 19,949189},
+			title: "Kraków, ul. Bujaka",
+			snippet: "Kraków, ul. Bujaka"
+		},
+		{
+			position: {lng: 50,081197, lat: 19,895358},
+			title: "Kraków, ul. Złoty Róg",
+			snippet: "Kraków, ul. Złoty Róg"
+		},
+		{
+			position: {lng: 50,069308, lat: 20,053492},
+			title: "Kraków, ul. Bulwarowa",
+			snippet: "Kraków, ul. Bulwarowa"
+		},
+		{
+			position: {lng: 50,099361, lat: 20,018317},
+			title: "Kraków, os. Piastów",
+			snippet: "Kraków, os. Piastów"
+		},
+		{
+			position: {lng: 50,0192 , lat: 20,016803},
+			title: "Kraków, ul. Telimeny",
+			snippet: "Kraków, ul. Telimeny"
+		},
+		{
+			position: {lng: 50,100569, lat: 20,122561},
+			title: "Kraków, os. Wadów",
+			snippet: "Kraków, os. Wadów"
+		}
+		];
+	
+		map.addMarkers(map, stations, function(markers) {
+			var bounds = [];
+			stations.forEach(function(POI) {
+				bounds.push(POI.position);
+			});
+			map.moveCamera({
+				target: bounds
+			}, function() {
+				markers[markers.length - 1].showInfoWindow();
+			});
 		});
 	});
 
@@ -94,11 +96,8 @@ map.one(plugin.google.maps.event.MAP_READY, function() {
 		stations.forEach(function(markerOptions) {
 			map.addMarker(markerOptions, onMarkerAdded);
 		});
-	}*/
-});
-
-/*map.one(plugin.google.maps.event.MAP_READY, function() {
-	map.on(plugin.google.maps.event.MY_LOCATION_BUTTON_CLICK, function() {
+	}
+	/*map.one(plugin.google.maps.event.MY_LOCATION_BUTTON_CLICK, function() {
 		function onSuccess(location) {
 			var msg = ["Current location:\n",
 			"latitude:" + location.latLng.lat,
@@ -106,15 +105,15 @@ map.one(plugin.google.maps.event.MAP_READY, function() {
 			"speed:" + location.speed,
 			"time:" + location.time,
 			"bearing:" + location.bearing].join("\n");
-		
+
 			map.addMarker({
 				'position': location.latLng,
-				title: msg
+				'title': msg
 			}, function(marker) {
 				marker.showInfoWindow();
 				map.animateCamera({
-					target: location.latLng,
-					zoom: 16
+				target: location.latLng,
+				zoom: 16
 				}, function() {
 					marker.showInfoWindow();
 				});
@@ -127,7 +126,7 @@ map.one(plugin.google.maps.event.MAP_READY, function() {
 
 		map.clear();
 		map.getMyLocation(onSuccess, onError);
-	});
+	});	
 });
 
 function showMap() {
