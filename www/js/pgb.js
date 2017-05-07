f7da0dd633023c655cd02fedec99c235275d7e6f
Mykola Shchur <kolya_shchu@mail.ru>
2017-05-07 10:10:28 +0200

    clear

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -27,78 +27,9 @@ function onDeviceReady() {
 		}
 	});
 }
-map.one(plugin.google.maps.event.MAP_READY, function() {	
-	/*map.addMarker({
-		position: {lat: 50.057678, lng: 19.926189},
-		title: "Kraków, Aleja Krasińskiego",
-		snippet: "Kraków, Aleja Krasińskiego" 
-	}, function(marker) {
-		marker.showInfoWindow();
-	});
 
-/*var stations = [
-		{
-			position: {lat: 50.057678, lng: 19.926189},
-			title: "Kraków, Aleja Krasińskiego",
-			snippet: "Kraków, Aleja Krasińskiego" 
-		},
-		{
-			position: {lat: 50.057447, lng: 19.946008},
-			title: "Kraków, ul. Dietla",
-			snippet: "Kraków, ul. Dietla"
-		},
-		{
-			position: {lat: 50.010575, lng: 19.949189},
-			title: "Kraków, ul. Bujaka",
-			snippet: "Kraków, ul. Bujaka"
-		},
-		{
-			position: {lat: 50.081197, lng: 19.895358},
-			title: "Kraków, ul. Złoty Róg",
-			snippet: "Kraków, ul. Złoty Róg"
-		},
-		{
-			position: {lat: 50.069308, lng: 20.053492},
-			title: "Kraków, ul. Bulwarowa",
-			snippet: "Kraków, ul. Bulwarowa"
-		},
-		{
-			position: {lat: 50.099361, lng: 20.018317},
-			title: "Kraków, os. Piastów",
-			snippet: "Kraków, os. Piastów"
-		},
-		{
-			position: {lat: 50.0192, lng: 20.016803},
-			title: "Kraków, ul. Telimeny",
-			snippet: "Kraków, ul. Telimeny"
-		},
-		{
-			position: {lat: 50.100569, lng: 20.122561},
-			title: "Kraków, os. Wadów",
-			snippet: "Kraków, os. Wadów"
-		}
-	];*/
-		
-	/*for (var i = 0; i < stations.length; i++) {
-		map.addMarker(stations[i], function(marker) {
-		marker.showInfoWindow();
-		});
-	}*/
-
-	/*function addMarkers(map, stations, callback) {
-		var markers = [];
-		function onMarkerAdded(marker) {
-			markers.push(marker);
-			if (markers.length === stations.length) {
-				callback(markers);
-			}
-		}
-		stations.forEach(function(markerOptions) {
-			map.addMarker(markerOptions, onMarkerAdded);
-		});
-	}*/
-
-	map.two(plugin.google.maps.event.MY_LOCATION_BUTTON_CLICK, function() {
+map.one(plugin.google.maps.event.MAP_READY, function() {
+	map.on(plugin.google.maps.event.MY_LOCATION_BUTTON_CLICK, function() {
 		function onSuccess(location) {
 			var msg = ["Current location:\n",
 			"latitude:" + location.latLng.lat,
@@ -113,8 +44,8 @@ map.one(plugin.google.maps.event.MAP_READY, function() {
 			}, function(marker) {
 				marker.showInfoWindow();
 				map.animateCamera({
-					target: location.latLng,
-					zoom: 16
+				target: location.latLng,
+				zoom: 16
 				}, function() {
 					marker.showInfoWindow();
 				});
