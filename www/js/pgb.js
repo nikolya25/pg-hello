7580d037cdaebdac7368602b462800bdeab2f73d
Mykola Shchur <kolya_shchu@mail.ru>
2017-05-07 07:21:20 +0200

    map not on ready

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -6,81 +6,81 @@ function onDeviceReady() {
 	document.getElementById('mapshowbutton').style.display = "none";
 }	
 
-	var div = document.getElementById("map");
-	var map = plugin.google.maps.Map.getMap(div , {
-		'mapType': plugin.google.maps.MapTypeId.ROADMAP,
-		'controls': {
-			'compass': true,
-			'myLocationButton': true,
-			'indoorPicker': true,
-			'zoom': true
+var div = document.getElementById("map");
+var map = plugin.google.maps.Map.getMap(div);
+map.one(plugin.google.mapsevent.MAP_READY, function() {
+	'mapType': plugin.google.maps.MapTypeId.ROADMAP,
+	'controls': {
+		'compass': true,
+		'myLocationButton': true,
+		'indoorPicker': true,
+		'zoom': true
+	},
+	'gestures': {
+		'scroll': true,
+		'tilt': true,
+		'rotate': true,
+		'zoom': true
+	},
+	camera: {
+		target : {
+			lat: 50.0593677, lng: 19.9375843
 		},
-		'gestures': {
-			'scroll': true,
-			'tilt': true,
-			'rotate': true,
-			'zoom': true
-		},
-		camera: {
-			target : {
-				lat: 50.0593677, lng: 19.9375843
-			},
-			zoom : 14
-		}
+		zoom : 14
+	};
 	
-		var stations = [
-		{
-			position: {lng: 50,057678, lat: 19,926189},
-			title: "Kraków, Aleja Krasińskiego",
-			snippet: "Kraków, Aleja Krasińskiego" 
-		},
-		{
-			position: {lng: 50,057447, lat: 19,946008},
-			title: "Kraków, ul. Dietla",
-			snippet: "Kraków, ul. Dietla"
-		},
-		{
-			position: {lng: 50,010575, lat: 19,949189},
-			title: "Kraków, ul. Bujaka",
-			snippet: "Kraków, ul. Bujaka"
-		},
-		{
-			position: {lng: 50,081197, lat: 19,895358},
-			title: "Kraków, ul. Złoty Róg",
-			snippet: "Kraków, ul. Złoty Róg"
-		},
-		{
-			position: {lng: 50,069308, lat: 20,053492},
-			title: "Kraków, ul. Bulwarowa",
-			snippet: "Kraków, ul. Bulwarowa"
-		},
-		{
-			position: {lng: 50,099361, lat: 20,018317},
-			title: "Kraków, os. Piastów",
-			snippet: "Kraków, os. Piastów"
-		},
-		{
-			position: {lng: 50,0192 , lat: 20,016803},
-			title: "Kraków, ul. Telimeny",
-			snippet: "Kraków, ul. Telimeny"
-		},
-		{
-			position: {lng: 50,100569, lat: 20,122561},
-			title: "Kraków, os. Wadów",
-			snippet: "Kraków, os. Wadów"
-		}
-		];
-	
-		map.addMarkers(map, stations, function(markers) {
-			var bounds = [];
-			stations.forEach(function(POI) {
-				bounds.push(POI.position);
-			});
-			map.moveCamera({
-				target: bounds
-			}, function() {
-				markers[markers.length - 1].showInfoWindow();
-			});
+	/*var stations = [
+	{
+		position: {lng: 50,057678, lat: 19,926189},
+		title: "Kraków, Aleja Krasińskiego",
+		snippet: "Kraków, Aleja Krasińskiego" 
+	},
+	{
+		position: {lng: 50,057447, lat: 19,946008},
+		title: "Kraków, ul. Dietla",
+		snippet: "Kraków, ul. Dietla"
+	},
+	{
+		position: {lng: 50,010575, lat: 19,949189},
+		title: "Kraków, ul. Bujaka",
+		snippet: "Kraków, ul. Bujaka"
+	},
+	{
+		position: {lng: 50,081197, lat: 19,895358},
+		title: "Kraków, ul. Złoty Róg",
+		snippet: "Kraków, ul. Złoty Róg"
+	},
+	{
+		position: {lng: 50,069308, lat: 20,053492},
+		title: "Kraków, ul. Bulwarowa",
+		snippet: "Kraków, ul. Bulwarowa"
+	},
+	{
+		position: {lng: 50,099361, lat: 20,018317},
+		title: "Kraków, os. Piastów",
+		snippet: "Kraków, os. Piastów"
+	},
+	{
+		position: {lng: 50,0192 , lat: 20,016803},
+		title: "Kraków, ul. Telimeny",
+		snippet: "Kraków, ul. Telimeny"
+	},
+	{
+		position: {lng: 50,100569, lat: 20,122561},
+		title: "Kraków, os. Wadów",
+		snippet: "Kraków, os. Wadów"
+	}
+	];
+
+	map.addMarkers(map, stations, function(markers) {
+		var bounds = [];
+		stations.forEach(function(POI) {
+			bounds.push(POI.position);
+		});
+		map.moveCamera({
+			target: bounds
+		}, function() {
+			markers[markers.length - 1].showInfoWindow();
 		});
 	});
 
@@ -95,7 +95,8 @@ function onDeviceReady() {
 		stations.forEach(function(markerOptions) {
 			map.addMarker(markerOptions, onMarkerAdded);
 		});
-	}
+	}*/
+});
 
 map.one(plugin.google.maps.event.MAP_READY, function() {
 	map.on(plugin.google.maps.event.MY_LOCATION_BUTTON_CLICK, function() {
@@ -127,7 +128,7 @@ map.one(plugin.google.maps.event.MAP_READY, function() {
 
 		map.clear();
 		map.getMyLocation(onSuccess, onError);
-	});	
+	});
 });
 
 function showMap() {
